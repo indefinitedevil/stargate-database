d5e49200a7464408c98f2db2dd2ecf246d20ef79
Eligos Alexander <eligos.alexander@reasondigital.com>
2024-11-10 00:07:56 +0000

    progress

--- resources/views/characters/view.blade.php
+++ resources/views/characters/view.blade.php
@@ -31,10 +31,13 @@
 
             <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg text-gray-800 dark:text-gray-300">
                 <div class="">
+                    <a href="{{ route('characters.edit', ['characterId' => $character->id]) }}"
+                       class="float-right px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150"
+                    >{{ __('Edit Skills') }}</a>
                     <h2 class="text-xl font-medium text-gray-900 dark:text-gray-100">
                         {{ __('Skills') }}
                     </h2>
-                    <div class="grid grid-cols-4">
+                    <div class="grid grid-cols-4 clear-both">
                         <div class="mt-1">
                             <h3 class="text-lg font-medium text-gray-900 dark:text-gray-100">{{ __('Background') }}</h3>
                             <ul>
