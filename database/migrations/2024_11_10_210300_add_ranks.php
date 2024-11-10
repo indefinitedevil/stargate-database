f1d8d1d6ad788c43229a8db160b56fc3153f3834
Eligos Alexander <eligos.alexander@reasondigital.com>
2024-11-10 21:20:55 +0000

    add ranks and fix editing

---
+++ database/migrations/2024_11_10_210300_add_ranks.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('characters', function (Blueprint $table) {
+            $table->string('former_rank', 64)->default('');
+            $table->string('rank', 64)->default('');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
