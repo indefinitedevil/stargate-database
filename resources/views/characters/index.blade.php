4a26c131fe06d3e7ef0be6550434b108661f128d
Eligos Alexander <eligos.alexander@reasondigital.com>
2024-11-25 21:19:28 +0000

    add create character button that was definitely never missing

--- resources/views/characters/index.blade.php
+++ resources/views/characters/index.blade.php
@@ -1,6 +1,9 @@
 <x-app-layout>
     <x-slot name="title">{{ __('My characters') }}</x-slot>
     <x-slot name="header">
+        <a href="{{ route('characters.create') }}"
+           class="float-right px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150"
+        >{{ __('Create') }}</a>
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('My characters') }}
         </h2>
