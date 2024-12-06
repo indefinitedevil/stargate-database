7ea8739d5493ad184b94cedad6404c780a546a71
Eligos Alexander <eligos.alexander@reasondigital.com>
2024-12-06 17:58:37 +0000

    fix guest views

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('dashboard');
+    return view('welcome');
 });
 
 Route::get('/dashboard', function () {
