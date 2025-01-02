2684ac8ca291de9aad585e3025136adfee7f24ee
Eligos Alexander <eligos.alexander@reasondigital.com>
2025-01-02 18:30:24 +0000

    fix ready for approval state action

--- resources/views/characters/partials/actions.blade.php
+++ resources/views/characters/partials/actions.blade.php
@@ -45,10 +45,12 @@
     ><i class="fa-solid fa-eye"></i></a>
 @endif
 @can('edit', $character)
-    <a href="{{ route('characters.ready', ['characterId' => $character->id]) }}"
-       class="float-right px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150 ml-1"
-       title="{{ __('Confirm ready for approval') }}"
-    ><i class="fa-solid fa-check"></i></a>
+    @if($character->status_id === Status::NEW)
+        <a href="{{ route('characters.ready', ['characterId' => $character->id]) }}"
+           class="float-right px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150 ml-1"
+           title="{{ __('Confirm ready for approval') }}"
+        ><i class="fa-solid fa-check"></i></a>
+    @endif
 @endcan
 @can('approve', $character)
     <a href="{{ route('characters.deny', ['characterId' => $character->id]) }}"
