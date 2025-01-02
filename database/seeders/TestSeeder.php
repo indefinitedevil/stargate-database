3b64f29d3317012f524e73d82a9ef7207c2d64d5
Eligos Alexander <eligos.alexander@reasondigital.com>
2025-01-02 22:08:05 +0000

    disable test user

--- database/seeders/TestSeeder.php
+++ database/seeders/TestSeeder.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+use Nette\Utils\Random;
 
 class TestSeeder extends Seeder
 {
@@ -14,11 +15,19 @@ class TestSeeder extends Seeder
      */
     public function run(): void
     {
+        /*
         DB::table('users')->insertOrIgnore([
             'name' => 'Test User',
             'email' => 'test@example.com',
             'password' => Hash::make('password'),
         ]);
+        */
+        DB::table('users')->upsert([
+            [
+                'email' => 'test@example.com',
+                'password' => Hash::make(Random::generate()),
+            ]
+        ], 'email');
         foreach (User::all() as $user) {
             $user->assignRole('player');
         }
