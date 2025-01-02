aaa60fa90b1bcaaf7c12653454ea903637d0500d
Eligos Alexander <eligos.alexander@reasondigital.com>
2025-01-02 13:30:28 +0000

    reorder details

--- resources/views/characters/partials/details.blade.php
+++ resources/views/characters/partials/details.blade.php
@@ -1,12 +1,9 @@
 <div class="grid grid-cols-4">
     <p class="mt-1">
-        <strong>{{ __('Name') }}:</strong> {{ $character->name }}
+        <strong>{{ __('Background') }}:</strong> {{ $character->background->name }}
     </p>
     <p class="mt-1">
-        <strong>{{ __('Rank') }}
-            :</strong> {!! $character->rank ?: '<abbr title="To be determined">TBD</abbr>' !!} @if ($character->former_rank)
-            ({{ $character->former_rank }})
-        @endif
+        <strong>{{ __('Status') }}:</strong> {{ $character->status->name }}
     </p>
     <p class="mt-1">
         <strong>{{ __('Body') }}:</strong> {{ $character->body }}
@@ -14,10 +11,10 @@
     <p class="mt-1">
         <strong>{{ __('Vigor') }}:</strong> {{ $character->vigor }}
     </p>
-    <p class="mt-1">
-        <strong>{{ __('Background') }}:</strong> {{ $character->background->name }}
-    </p>
-    <p class="mt-1">
-        <strong>{{ __('Status') }}:</strong> {{ $character->status->name }}
+    <p class="mt-1 col-span-2">
+        <strong>{{ __('Rank') }}
+            :</strong> {!! $character->rank ?: '<abbr title="To be determined">TBD</abbr>' !!} @if ($character->former_rank)
+            ({{ $character->former_rank }})
+        @endif
     </p>
 </div>
