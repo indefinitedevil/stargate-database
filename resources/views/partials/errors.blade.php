7694947e31dba6e7066234e3e10bfddc8f6dbb10
Eligos Alexander <eligos.alexander@reasondigital.com>
2025-01-02 14:37:23 +0000

    fix error display

--- resources/views/partials/errors.blade.php
+++ resources/views/partials/errors.blade.php
@@ -1,11 +1,9 @@
 @if ($errors->any())
-    <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg text-gray-800 dark:text-gray-300">
-        <div class="bg-orange-100 border-l-4 border-orange-500 text-orange-700 p-4">
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
+    <div class="bg-orange-100 border-l-4 border-orange-500 text-orange-700 p-4 shadow">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
     </div>
 @endif
