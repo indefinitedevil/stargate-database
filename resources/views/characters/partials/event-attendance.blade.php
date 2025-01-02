82c1116330fe90cb39e0bfbb0de877cb23914d23
Eligos Alexander <eligos.alexander@reasondigital.com>
2025-01-02 23:39:29 +0000

    fix event attendance widget

--- resources/views/characters/partials/event-attendance.blade.php
+++ resources/views/characters/partials/event-attendance.blade.php
@@ -3,13 +3,13 @@
 @endphp
 <div>
     <p>This is a temporary measure, but if you could indicate which events you intend to
-        attend as this character, that would help event runners greatly.</p>
+        attend as this character, that would help the event runners and plot cos greatly.</p>
     <div class="grid grid-cols-4">
         @foreach(Event::all() as $event)
             <label>
                 <input type="checkbox" id="event_{{ $event->id }}" name="events[]"
                        value="{{ $event->id }}"
-                       @if($character->events->contains($event->id)) checked @endif />
+                       @if(!empty($character) && $character->events->contains($event->id)) checked @endif />
                 {{ $event->name}}
                 ({{ $event->start_date->format('d/m/y') }} - {{ $event->end_date->format('d/m/y') }})
             </label>
