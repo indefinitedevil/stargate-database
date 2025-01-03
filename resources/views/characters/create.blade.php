e4b9668dbe79c307c0eb23d6164c86923fbe8f88
Eligos Alexander <eligos.alexander@reasondigital.com>
2025-01-03 01:03:29 +0000

    update buttons

--- resources/views/characters/create.blade.php
+++ resources/views/characters/create.blade.php
@@ -48,10 +48,7 @@
                             </div>
 
                             <div class="flex items-center gap-4">
-                                <button type="submit"
-                                        class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">
-                                    {{ __('Create') }}
-                                </button>
+                                <x-primary-button>{{ __('Create') }}</x-primary-button>
                             </div>
                         </div>
                     </form>
@@ -59,7 +56,7 @@
             </div>
             <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg text-gray-800 dark:text-gray-300">
                 <div class="mt-1">
-                    <p>Once you've created your character, you can move onto adding skills. You can come back and edit these details afterwards if you need to.</p>
+                    <p>{{ __('Once you\'ve created your character, you can move onto adding skills. You can come back and edit these details afterwards if you need to.') }}</p>
                 </div>
             </div>
         </div>
