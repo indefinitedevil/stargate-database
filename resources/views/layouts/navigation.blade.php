3ade310d86bcf1264d5cd1fb3788f920307ad4a3
Eligos Alexander <eligos.alexander@reasondigital.com>
2025-01-06 12:52:49 +0000

    add notice that name is not unique

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -79,7 +79,12 @@
                         <x-slot name="trigger">
                             <button
                                 class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
-                                <div>{{ Auth::user()->name }}</div>
+                                <div>
+                                    {{ Auth::user()->name }}
+                                    @if (!Auth::user()->isNameUnique())
+                                        <i class="fa-solid fa-circle-exclamation text-red-500"></i>
+                                    @endif
+                                </div>
 
                                 <div class="ms-1">
                                     <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg"
@@ -95,6 +100,9 @@
                         <x-slot name="content">
                             <x-dropdown-link :href="route('profile.edit')">
                                 {{ __('Profile') }}
+                                @if (!Auth::user()->isNameUnique())
+                                    <i class="fa-solid fa-circle-exclamation text-red-500"></i>
+                                @endif
                             </x-dropdown-link>
 
                             <!-- Authentication -->
@@ -175,6 +183,9 @@
                 <div class="mt-3 space-y-1">
                     <x-responsive-nav-link :href="route('profile.edit')">
                         {{ __('Profile') }}
+                        @if (!Auth::user()->isNameUnique())
+                            <i class="fa-solid fa-circle-exclamation text-red-500"></i>
+                        @endif
                     </x-responsive-nav-link>
 
                     <!-- Authentication -->
--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -7,6 +7,14 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+            @if (!$user->isNameUnique())
+                <div class="bg-orange-100 border-l-4 border-orange-500 text-orange-700 p-4 shadow">
+                    <ul>
+                        {{ __('Your name is not unique which may make it difficult to distinguish you from other players.') }}
+                    </ul>
+                </div>
+            @endif
+
             <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
                 <div class="max-w-xl">
                     @include('profile.partials.update-profile-information-form')
