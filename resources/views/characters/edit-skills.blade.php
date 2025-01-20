2c78252327ce9db436b46ece8d84124164d853d9
Eligos Alexander <eligos.alexander@reasondigital.com>
2025-01-20 21:30:40 +0000

    separate available skills into partial in case we want to include it in downtime selection

--- resources/views/characters/edit-skills.blade.php
+++ resources/views/characters/edit-skills.blade.php
@@ -199,30 +199,7 @@
                                             @else
                                                 <option value="">{{ __('Select a skill') }}</option>
                                             @endif
-                                            @foreach($character->availableSkills as $skill)
-                                                @if (empty($currentCategory) || $currentCategory != $skill->skill_category_id)
-                                                    @if (!empty($currentCategory))
-                                                        {!! '</optgroup>' !!}
-                                                    @endif
-                                                    @php $currentCategory = $skill->skill_category_id; @endphp
-                                                    {!! '<optgroup label="' . __(':name Skills', ['name' => $skill->skillCategory->name]) . '">' !!}
-                                                @endif
-                                                @php
-                                                    $skills[] = $skill;
-                                                    if ($skill->repeatable) {
-                                                        $count = $character->skills->where('skill_id', $skill->id)->count();
-                                                        if ($count >= $skill->repeatable) {
-                                                            continue;
-                                                        }
-                                                    }
-                                                @endphp
-                                                <option value="{{ $skill->id }}">
-                                                    {{ __(':name (:cost months)', ['name' => $skill->name, 'cost' => $skill->cost($character)]) }}
-                                                </option>
-                                            @endforeach
-                                            @if (!empty($currentCategory))
-                                                {!! '</optgroup>' !!}
-                                            @endif
+                                            @include('characters.partials.available-skills')
                                         </x-select>
                                     @endif
                                 </div>
---
+++ resources/views/characters/partials/available-skills.blade.php
@@ -0,0 +1,24 @@
+@foreach($character->availableSkills as $skill)
+    @if (empty($currentCategory) || $currentCategory != $skill->skill_category_id)
+        @if (!empty($currentCategory))
+            {!! '</optgroup>' !!}
+        @endif
+        @php $currentCategory = $skill->skill_category_id; @endphp
+        {!! '<optgroup label="' . __(':name Skills', ['name' => $skill->skillCategory->name]) . '">' !!}
+    @endif
+    @php
+        $skills[] = $skill;
+        if ($skill->repeatable) {
+            $count = $character->skills->where('skill_id', $skill->id)->count();
+            if ($count >= $skill->repeatable) {
+                continue;
+            }
+        }
+    @endphp
+    <option value="{{ $skill->id }}">
+        {{ __(':name (:cost months)', ['name' => $skill->name, 'cost' => $skill->cost($character)]) }}
+    </option>
+@endforeach
+@if (!empty($currentCategory))
+    {!! '</optgroup>' !!}
+@endif
