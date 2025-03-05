918fe52142998d94160c2596458fce31950c6841
Eligos Alexander <eligos.alexander@reasondigital.com>
2025-03-05 22:01:19 +0000

    update reset wording for #38 and add it to downtime submission page

--- resources/views/downtimes/submit.blade.php
+++ resources/views/downtimes/submit.blade.php
@@ -37,6 +37,8 @@
                     </div>
                 </div>
 
+                @include('characters.partials.reset')
+
                 <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 text-gray-900 dark:text-gray-100">
                         <a href="{{ route('characters.edit-skills', ['characterId' => $character->id]) }}"
