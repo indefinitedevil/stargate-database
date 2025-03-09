7c893428126b1beabea05ab1ae6b0633815d9a4a
Eligos Alexander <eligos.alexander@reasondigital.com>
2025-03-09 20:52:25 +0000

    fix permissions for creating downtimes #29

--- resources/views/plotco/downtimes/index.blade.php
+++ resources/views/plotco/downtimes/index.blade.php
@@ -1,11 +1,7 @@
-@php
-    use App\Models\Downtime;
-    use Illuminate\Support\Facades\Auth;
-@endphp
 <x-app-layout>
     <x-slot name="title">{{ __('Downtimes') }}</x-slot>
     <x-slot name="header">
-        @can('edit events')
+        @can('edit downtimes')
             <a href="{{ route('plotco.downtimes.create') }}"
                class="float-right px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150"
             >{{ __('Create') }}</a>
