2a6dfbf503804908ddeed9a0d43f22428abdc66f
Eligos Alexander <eligos.alexander@reasondigital.com>
2025-04-08 20:02:33 +0100

    fix attendance permissions

--- resources/views/plotco/attendance.blade.php
+++ resources/views/plotco/attendance.blade.php
@@ -11,6 +11,7 @@
 
     @foreach (Event::where('end_date', '>', date('Y-m-d'))->get() as $event)
         <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg text-gray-800 dark:text-gray-300">
+            @can('view all characters')
             <a href="{{ route('plotco.print-some', ['event' => $event->id]) }}"
                class="float-right px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150"
                title="{{ __('Print Characters') }}"
@@ -18,6 +19,7 @@
                 <i class="fa-solid fa-print"></i>
                 {{ __('Print') }}
             </a>
+            @endcan
             @can('record attendance')
                 <a href="{{ route('events.attendance', $event) }}"
                    class="float-right px-4 py-2 mr-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150"
