84fe447d35573718bcdd3eb2b7f9ec110f5d6938
Eligos Alexander <eligos.alexander@reasondigital.com>
2025-04-12 20:38:44 +0100

    fix email header

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,11 +2,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-@if (trim($slot) === 'Laravel')
-<img src="{{ asset('/images/sef-logo-112.png') }}" class="logo" alt="Stargate Logo">
-@else
-{{ $slot }}
-@endif
+<img src="{{ asset('/images/sef-logo-112.png') }}" class="logo" alt="{{ $slot }}">
 </a>
 </td>
 </tr>
