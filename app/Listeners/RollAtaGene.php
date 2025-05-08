b3b50a7322b4bd8672436dd41d55edc8302fb60b
Eligos Alexander <eligos.alexander@reasondigital.com>
2025-05-08 21:28:39 +0100

    use better random generator? #77

--- app/Listeners/RollAtaGene.php
+++ app/Listeners/RollAtaGene.php
@@ -20,7 +20,11 @@ class RollAtaGene
     public function handle(CharacterApproved $event): void
     {
         if ($event->character->ata_gene < 0) {
-            $roll = rand(1, 20);
+            try {
+                $roll = random_int(1, 20);
+            } catch (\Throwable $th) {
+                $roll = rand(1, 20);
+            }
             if (20 == $roll) {
                 $event->character->ata_gene = 1;
             } else {
