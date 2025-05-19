9f848bb8c7814cbdfc370e75440dd28906652d4e
Eligos Alexander <eligos.alexander@reasondigital.com>
2025-05-19 22:28:20 +0100

    fix display bug

--- app/Models/Character.php
+++ app/Models/Character.php
@@ -214,7 +214,7 @@ class Character extends Model
         $requiredUpkeepSkills = [];
         foreach ($upkeepSkills as $upkeepSkill) {
             if (1 < $upkeepSkill->level) {
-                $requiredUpkeepSkills[] = $upkeepSkill;
+                $requiredUpkeepSkills[$upkeepSkill->skill_id] = $upkeepSkill;
             }
         }
         return collect($requiredUpkeepSkills);
