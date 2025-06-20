bbe5a0b6cbf8086700b294cf1e6a8ab39d818b8d
Eligos Alexander <eligos.alexander@reasondigital.com>
2025-06-20 22:26:31 +0100

    initial pass at rules update - #87

---
+++ database/migrations/2025_06_20_204728_rules_1.3_update.php
@@ -0,0 +1,38 @@
+<?php
+
+use Database\Seeders\FeatSeeder;
+use Database\Seeders\SkillSeeder;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('feats', function ($table) {
+            $table->string('print_name')->nullable()->after('name');
+            $table->tinyInteger('per_day')->default(0)->after('per_event');
+        });
+
+        // Add skill adjustments
+        $seeder = new SkillSeeder();
+        $seeder->run();
+
+        // Add feat adjustments
+        $seeder = new FeatSeeder();
+        $seeder->run();
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('feats', function ($table) {
+            $table->dropColumn('print_name');
+            $table->dropColumn('per_day');
+        });
+    }
+};
