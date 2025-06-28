eba896724589d461200baf608d8b7a6a20a09aa2
Eligos Alexander <eligos.alexander@reasondigital.com>
2025-06-28 22:01:59 +0100

    action type clarity update

--- app/Models/ActionType.php
+++ app/Models/ActionType.php
@@ -5,24 +5,31 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Collection;
 
+/**
+ * @property int id
+ * @property int type
+ * @property string name
+ * @property DowntimeAction[]|Collection downtimeActions
+ */
 class ActionType extends Model
 {
     use HasFactory;
 
-    const DEVELOPMENT = 1;
-    const RESEARCH = 2;
-    const MISC = 3;
-    const EXPERIMENT = 4;
+    const TYPE_DEVELOPMENT = 1;
+    const TYPE_RESEARCH = 2;
+    const TYPE_MISC = 3;
+    const TYPE_EXPERIMENT = 4;
 
-    const TRAINING = 1;
-    const TEACHING = 2;
-    const UPKEEP = 3;
-    const MISSION = 4;
-    const RESEARCHING = 5;
-    const UPKEEP_2 = 6;
-    const OTHER = 7;
-    const RESEARCH_SUBJECT = 8;
+    const ACTION_TRAINING = 1;
+    const ACTION_TEACHING = 2;
+    const ACTION_UPKEEP = 3;
+    const ACTION_MISSION = 4;
+    const ACTION_RESEARCHING = 5;
+    const ACTION_UPKEEP_2 = 6;
+    const ACTION_OTHER = 7;
+    const ACTION_RESEARCH_SUBJECT = 8;
 
     protected $fillable = [
         'type',
