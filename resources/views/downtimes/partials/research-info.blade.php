a53cbc379918e5d1912353478dd56941796723cf
Eligos Alexander <eligos.alexander@reasondigital.com>
2025-06-29 23:15:14 +0100

    fix research display on downtimes - #77

--- resources/views/downtimes/partials/research-info.blade.php
+++ resources/views/downtimes/partials/research-info.blade.php
@@ -1,6 +1,6 @@
 @if($downtime->isOpen() && $downtime->researchProjects->count() > 0)
     <div
-        class="p-6 bg-white dark:bg-gray-800 shadow sm:rounded-lg text-gray-800 dark:text-gray-300 row-span-{{ $downtime->research_actions + $downtime->experiment_actions }}">
+            class="p-6 bg-white dark:bg-gray-800 shadow sm:rounded-lg text-gray-800 dark:text-gray-300 row-span-{{ $downtime->research_actions + $downtime->experiment_actions }}">
         <div>
             <h3 class="text-lg font-semibold">{{ __('Research Projects') }}</h3>
             <div class="space-y-2">
@@ -8,9 +8,21 @@
                 <ul class="list-inside list-disc">
                     @foreach($downtime->researchProjects as $researchProject)
                         <li>
-                            <strong>{{ $researchProject->name }}</strong>
+                            <a href="{{ $researchProject->getViewRoute() }}"
+                               class="underline"><strong>{!! process_inline_markdown($researchProject->name) !!}</strong></a>
                             {{ __('(:done / :total months)', ['done' => $researchProject->researchActions()->count(), 'total' => $researchProject->months]) }}
-                            <p class="ml-5">{{ __('Subject: :subject', ['subject' => $researchProject->research_subject]) }}</p>
+                            (<a class="cursor-pointer underline decoration-dashed underline-offset-4" onclick="toggleVisibility('research-skills-{{ $researchProject->id }}')">{{ __('Show skills') }}</a>)
+                            <p class="ml-5">{!! process_inline_markdown(__('Subject: :subject', ['subject' => $researchProject->research_subject])) !!}</p>
+                            <p class="ml-5 hidden" id="research-skills-{{ $researchProject->id }}">
+                                <strong>{{ __('Skills:') }}</strong>
+                                @if ($researchProject->skills->count() > 0)
+                                    @foreach($researchProject->skills as $skill)
+                                        {{ $skill->name }}{{ $loop->last ? '' : ', ' }}
+                                    @endforeach
+                                @else
+                                    {{ __('None specified') }}
+                                @endif
+                            </p>
                             @php $researchActions = $researchProject->researchActions()->where('downtime_id', $downtime->id)->get(); @endphp
                             @if ($researchActions->count() > 0)
                                 <ul class="list-inside list-disc ml-4">
