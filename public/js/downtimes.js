9ad5358b785e1404e8ec5999cfc08c42982aea03
Eligos Alexander <eligos.alexander@reasondigital.com>
2025-07-03 21:39:32 +0100

    improve js

--- public/js/downtimes.js
+++ public/js/downtimes.js
@@ -1,12 +1,19 @@
+const ACTION_TRAINING = '1';
+const ACTION_TEACHING = '2';
+const ACTION_UPKEEP = '3';
+const ACTION_MISSION = '4';
+const ACTION_UPKEEP_2 = '5';
+const ACTION_RESEARCH = '6';
+
 jQuery('[id^="development_action_"]').on('change', function () {
     let id = jQuery(this).attr('id').split('_').pop();
     jQuery('[id^="development_skill_' + id + '"]').html(jQuery('#ds_' + id + '_' + jQuery(this).val()).html());
-    if (jQuery(this).val() == 4) { // Mission
+    if (jQuery(this).val() === ACTION_MISSION) { // Mission
         jQuery('#da_' + id + '_notes').removeClass('hidden');
     } else {
         jQuery('#da_' + id + '_notes').addClass('hidden');
     }
-    if (jQuery(this).val() == 2) { // Teaching
+    if (jQuery(this).val() === ACTION_TEACHING) { // Teaching
         jQuery('#da_' + id + '_teaching').removeClass('hidden');
     } else {
         jQuery('#da_' + id + '_teaching').addClass('hidden');
@@ -15,11 +22,11 @@ jQuery('[id^="development_action_"]').on('change', function () {
 
 jQuery('[id^="research_action_"]').on('change', function () {
     let id = jQuery(this).attr('id').split('_').pop();
-    if (jQuery(this).val() == 6) { // Upkeep 2
+    if (jQuery(this).val() === ACTION_UPKEEP_2) { // Upkeep 2
         jQuery('#upkeep_skill_' + id).removeClass('hidden');
         jQuery('#research_project_' + id).addClass('hidden');
         jQuery('#research_action_' + id + '_notes').addClass('hidden');
-    } else if (jQuery(this).val() == 5) { // Research
+    } else if (jQuery(this).val() === ACTION_RESEARCH) { // Research
         jQuery('#upkeep_skill_' + id).addClass('hidden');
         jQuery('#research_project_' + id).removeClass('hidden');
         if (jQuery('#research_project_' + id + ' option').length > 1) {
