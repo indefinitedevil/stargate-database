bae21472a63369b1f089347430acff3d1a220005
Eligos Alexander <eligos.alexander@reasondigital.com>
2025-07-07 22:54:00 +0100

    changelog

--- resources/views/changelog.blade.php
+++ resources/views/changelog.blade.php
@@ -9,6 +9,12 @@
     <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
         <div class="p-6 text-gray-900 dark:text-gray-100 space-y-2">
             <div>
+                <h3 class="text-lg font-semibold">Bugfix: 7th July 2025</h3>
+                <ul class="list-inside list-disc">
+                    <li>Fixed downtime processing errors.</li>
+                </ul>
+            </div>
+            <div>
                 <h3 class="text-lg font-semibold">Update: 4th July 2025</h3>
                 <ul class="list-inside list-disc">
                     <li>Sys Ref: rules 1.3.1 update.</li>
