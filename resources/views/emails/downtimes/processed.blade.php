ab4a91b654a23497da142afdd888d7098a5ac2ef
Eligos Alexander <eligos.alexander@reasondigital.com>
2025-07-07 23:09:17 +0100

    fix downtime email

--- resources/views/emails/downtimes/processed.blade.php
+++ resources/views/emails/downtimes/processed.blade.php
@@ -5,61 +5,61 @@ The downtime has been processed, and here are your results:
 
 <ul>
 @foreach ($results as $result)
-    <li>
-        @if (!empty($result['notes']))
-            {!! process_markdown($result['notes']) !!}
-        @endif
-        @if (!empty($result['response']))
-            {!! process_markdown(__('Response: :response', ['response' => $result['response']])) !!}
-        @endif
-        @if (!empty($result['amount_trained']))
-            ({{ __('Trained :amount months', ['amount' => $result['amount_trained']]) }})
-            @if (!empty($result['skill_completed']))
-                ({{ __('Skill completed') }})
-            @endif
-        @endif
-        @if (!empty($result['body_change']))
-            ({{ __('Body :change', ['change' => add_positive_modifier($result['body_change'])]) }})
-        @endif
-        @if (!empty($result['vigor_change']))
-            ({{ __('Vigor :change', ['change' => add_positive_modifier($result['vigor_change'])]) }})
-        @endif
-        @if (!empty($result['temp_body_change']))
-            ({{ __('Body :change for next event', ['change' => add_positive_modifier($result['temp_body_change'])]) }})
-        @endif
-        @if (!empty($result['temp_vigor_change']))
-            ({{ __('Vigor :change for next event', ['change' => add_positive_modifier($result['temp_vigor_change'])]) }})
-        @endif
-    </li>
+<li>
+@if (!empty($result['notes']))
+{!! process_markdown($result['notes']) !!}
+@endif
+@if (!empty($result['response']))
+{!! process_markdown(__('Response: :response', ['response' => $result['response']])) !!}
+@endif
+@if (!empty($result['amount_trained']))
+({{ __('Trained :amount months', ['amount' => $result['amount_trained']]) }})
+@if (!empty($result['skill_completed']))
+({{ __('Skill completed') }})
+@endif
+@endif
+@if (!empty($result['body_change']))
+({{ __('Body :change', ['change' => add_positive_modifier($result['body_change'])]) }})
+@endif
+@if (!empty($result['vigor_change']))
+({{ __('Vigor :change', ['change' => add_positive_modifier($result['vigor_change'])]) }})
+@endif
+@if (!empty($result['temp_body_change']))
+({{ __('Body :change for next event', ['change' => add_positive_modifier($result['temp_body_change'])]) }})
+@endif
+@if (!empty($result['temp_vigor_change']))
+({{ __('Vigor :change for next event', ['change' => add_positive_modifier($result['temp_vigor_change'])]) }})
+@endif
+</li>
 @endforeach
 </ul>
 
 @if (!empty($researchResults))
 <h3>{{ __('Research Projects') }}</h3>
 <ul>
-    @foreach ($researchResults as $projectResult)
-        <li>
-            <strong>{!! process_inline_markdown($projectResult['project']->name) !!}</strong>:
-            {{ __('(:done / :total months)', ['done' => $projectResult['project']->researchActions()->count(), 'total' => $projectResult['project']->months]) }}
-            @if (!empty($projectResult['contributors']))
-                <ul>
-                    @foreach ($projectResult['contributors'] as $contributorId => $contributor)
-                        <li>{{ __('Researcher: :name (:months months)', ['name' => current($contributor), 'months' => count($contributor)]) }}</li>
-                    @endforeach
-                </ul>
-            @endif
-            @if (!empty($projectResult['volunteers']))
-                <ul>
-                    @foreach ($projectResult['volunteers'] as $volunteer)
-                        <li>{{ __('Volunteer: :name', ['name' => $volunteer]) }}</li>
-                    @endforeach
-                </ul>
-            @endif
-            @if (!empty($projectResult['results']))
-                {!! process_markdown(__('Results: :results', ['results' => $projectResult['results']])) !!}
-            @endif
-        </li>
-    @endforeach
+@foreach ($researchResults as $projectResult)
+<li>
+<strong>{!! process_inline_markdown($projectResult['project']->name) !!}</strong>:
+{{ __('(:done / :total months)', ['done' => $projectResult['project']->researchActions()->count(), 'total' => $projectResult['project']->months]) }}
+@if (!empty($projectResult['contributors']))
+<ul>
+@foreach ($projectResult['contributors'] as $contributorId => $contributor)
+<li>{{ __('Researcher: :name (:months months)', ['name' => current($contributor), 'months' => count($contributor)]) }}</li>
+@endforeach
+</ul>
+@endif
+@if (!empty($projectResult['volunteers']))
+<ul>
+@foreach ($projectResult['volunteers'] as $volunteer)
+<li>{{ __('Volunteer: :name', ['name' => $volunteer]) }}</li>
+@endforeach
+</ul>
+@endif
+@if (!empty($projectResult['results']))
+{!! process_markdown(__('Results: :results', ['results' => $projectResult['results']])) !!}
+@endif
+</li>
+@endforeach
 </ul>
 <x-mail::button :url="route('research.index')">{{ __('See research status') }}</x-mail::button>
 @endif
