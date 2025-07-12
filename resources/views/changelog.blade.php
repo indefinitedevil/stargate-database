653b52849ccfdf650807284846c5a1654f85c519
Eligos Alexander <eligos.alexander@reasondigital.com>
2025-07-12 17:34:51 +0100

    update changelog

--- resources/views/changelog.blade.php
+++ resources/views/changelog.blade.php
@@ -9,6 +9,16 @@
     <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
         <div class="p-6 text-gray-900 dark:text-gray-100 space-y-2">
             <div>
+                <h3 class="text-lg font-semibold">Update: 12th July 2025</h3>
+                <ul class="list-inside list-disc">
+                    <li>Added skill selection to research actions.</li>
+                    <li>Hid System skills from list of skills for teaching/researching.</li>
+                    <li>Updated error appearance on downtime submissions.</li>
+                    <li>Updated notes placeholders.</li>
+                    <li>Use base skill name on training course list.</li>
+                </ul>
+            </div>
+            <div>
                 <h3 class="text-lg font-semibold">Bugfix: 11th July 2025</h3>
                 <ul class="list-inside list-disc">
                     <li>Plot Co: added view as player option.</li>
