df0fbe9f21b94752d3560daaa3eb44eaa497a2ab
Eligos Alexander <eligos.alexander@reasondigital.com>
2025-07-29 17:44:09 +0100

    clean up ids

--- app/Models/Character.php
+++ app/Models/Character.php
@@ -238,7 +238,7 @@ class Character extends Model
     public function teachingSkills(): HasMany
     {
         return $this->trainedSkillsWithoutSystem()
-            ->where('id', '!=', 45);
+            ->where('id', '!=', Skill::LEADERSHIP_EXTRA_PERSON);
     }
 
     public function hiddenTrainedSkills(): HasMany
