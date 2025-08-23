930e5a622fc189dc8148d8bbc452c21655a09807
Eligos Alexander <eligos.alexander@reasondigital.com>
2025-08-23 20:11:31 +0100

    coderabbit changes

--- resources/views/organisation/teams/index.blade.php
+++ resources/views/organisation/teams/index.blade.php
@@ -35,7 +35,7 @@
                                 @else
                                     @foreach ($team->characters as $character)
                                         <li>
-                                            {{ $character->listName }}</a>
+                                            {{ $character->listName }}
                                             @if (Team::LEAD == $character->pivot->position)
                                                 <span class="text-sm text-gray-500">({{ __('Team Lead') }})</span>
                                             @endif
