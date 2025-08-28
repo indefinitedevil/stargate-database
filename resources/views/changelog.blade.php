f75a604bbb86315fd7df0ca36530f9b312960d91
Eligos Alexander <eligos.alexander@reasondigital.com>
2025-08-28 16:28:19 +0100

    changelog

--- resources/views/changelog.blade.php
+++ resources/views/changelog.blade.php
@@ -9,6 +9,12 @@
     <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm lg:rounded-lg">
         <div class="p-6 text-gray-900 dark:text-gray-100 space-y-2">
             <div>
+                <h3 class="text-lg font-semibold">Update: 28th August 2025</h3>
+                <ul class="list-inside list-disc">
+                    <li>Altered design philosophy to be closer to holy grail design.</li>
+                </ul>
+            </div>
+            <div>
                 <h3 class="text-lg font-semibold">Bugfix: 26th August 2025</h3>
                 <ul class="list-inside list-disc">
                     <li>Fixed research project ordering and increased per-page limit.</li>
