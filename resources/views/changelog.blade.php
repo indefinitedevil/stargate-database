27fe257e6c1a5da921395d28e092cbcc81a82f47
Eligos Alexander <eligos.alexander@reasondigital.com>
2025-08-28 20:30:16 +0100

    changelog

--- resources/views/changelog.blade.php
+++ resources/views/changelog.blade.php
@@ -9,6 +9,13 @@
     <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
         <div class="p-6 text-gray-900 dark:text-gray-100 space-y-2">
             <div>
+                <h3 class="text-lg font-semibold">Update: 28th August 2025</h3>
+                <ul class="list-inside list-disc">
+                    <li>Plot Co: added lowest-training character name to info box.</li>
+                    <li>Secretary: added toggle to hide non-booked people from attendance screen.</li>
+                </ul>
+            </div>
+            <div>
                 <h3 class="text-lg font-semibold">Bugfix: 27th August 2025</h3>
                 <ul class="list-inside list-disc">
                     <li>Fixed research project view for non-approved projects.</li>
