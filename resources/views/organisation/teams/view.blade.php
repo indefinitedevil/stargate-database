9233ba04293154babb0804aa0f02b6267a382fdf
Eligos Alexander <eligos.alexander@reasondigital.com>
2025-08-28 21:30:21 +0100

    holy grail teams

--- resources/views/organisation/teams/view.blade.php
+++ resources/views/organisation/teams/view.blade.php
@@ -1,15 +1,18 @@
-@php use App\Models\Team; @endphp
 <x-app-layout>
     <x-slot name="title">{{ __('Team :team', ['team' => $team->name]) }}</x-slot>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            @can ('edit teams')
-                <x-link-button href="{{ route('teams.edit', ['teamId' => $team]) }}"
-                               class="float-right">{{ __('Edit') }}</x-link-button>
-            @endcan
             {{ __('Team :team', ['team' => $team->name]) }}
         </h2>
     </x-slot>
+    @can('edit teams')
+        <x-slot name="sidebar2">
+            <x-dropdown-link href="{{ route('teams.edit', ['teamId' => $team]) }}">
+                <i class="fa-solid fa-pen min-w-8"></i>
+                {{ __('Edit') }}
+            </x-dropdown-link>
+        </x-slot>
+    @endcan
 
     <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow lg:rounded-lg text-gray-800 dark:text-gray-300">
         <div class="space-y-2">
