5bea67585b821d9608b51796995985756c7cd054
Eligos Alexander <eligos.alexander@reasondigital.com>
2025-08-28 20:00:51 +0100

    109: add toggle for booked people

--- resources/views/events/attendance.blade.php
+++ resources/views/events/attendance.blade.php
@@ -1,10 +1,12 @@
 @php
     use App\Models\Event;
-    use App\Models\User;
 @endphp
 <x-app-layout>
     <x-slot name="title">{{ __('Record attendance: :event', ['event' => $event->name]) }}</x-slot>
     <x-slot name="header">
+        <div class="sm:float-right">
+        <x-link-button onclick="toggleHideable()" class="cursor-pointer">{{ __('Toggle booked') }}</x-link-button>
+        </div>
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Record attendance: :event', ['event' => $event->name]) }}
         </h2>
@@ -12,14 +14,7 @@
 
     <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg text-gray-800 dark:text-gray-300">
         @php
-            $roles = [Event::ROLE_PLAYER, Event::ROLE_RUNNER, Event::ROLE_CREW, Event::ROLE_PAID_DOWNTIME];
             $cellClass = 'border-b border-slate-100 dark:border-slate-700 p-2';
-            $eventRoles = $attended = $characters = [];
-            foreach ($event->users as $user) {
-                $eventRoles[$user->id] = $user->pivot->role;
-                $attended[$user->id] = $user->pivot->attended;
-                $characters[$user->id] = $user->pivot->character_id;
-            }
         @endphp
         <form method="POST" action="{{ route('events.store-attendance') }}">
             @csrf
@@ -33,8 +28,8 @@
                 </tr>
                 </thead>
                 <tbody>
-                @foreach (User::orderBy('name', 'asc')->get() as $user)
-                    <tr>
+                @foreach ($users as $user)
+                    <tr class="{{ empty($attended[$user->id]) ? 'hideable' : '' }}">
                         <td class="{{ $cellClass }}">
                             <label>
                                 <input type="checkbox" name="attendance[{{ $user->id }}][attended]"
@@ -48,7 +43,7 @@
                                 <option value="0" {{ empty($eventRoles[$user->id]) ? 'selected' : '' }}>
                                     {{ __('None') }}
                                 </option>
-                                @foreach($roles as $role)
+                                @foreach ($roles as $role)
                                     <option value="{{ $role }}"
                                         {{ !empty($eventRoles[$user->id]) && $eventRoles[$user->id] == $role ? 'selected' : '' }}>
                                         {{ Event::roleName($role) }}
@@ -57,7 +52,7 @@
                             </x-select>
                             <x-select name="attendance[{{ $user->id }}][character_id]" class="w-full sm:w-1/2">
                                 <option value="">{{ __('None') }}</option>
-                                @foreach($user->characters as $character)
+                                @foreach ($user->characters as $character)
                                     <option value="{{ $character->id }}"
                                         {{ !empty($characters[$user->id]) && $characters[$user->id] == $character->id ? 'selected' : '' }}>
                                         {{ $character->listName }}
@@ -76,4 +71,5 @@
             </div>
         </form>
     </div>
+    <script src="{{ asset('js/events.js') }}" defer></script>
 </x-app-layout>
