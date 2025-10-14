4c5d8c49b469c6570a132bba5dbc3e79b6d5c3a3
Eligos Alexander <eligos.alexander@reasondigital.com>
2025-10-14 11:55:10 +0100

    add per_restore flag for #112

---
+++ database/migrations/2025_10_14_105026_add_per_restore_flag.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('skills', function (Blueprint $table) {
+            if (Schema::hasColumn('skills', 'per_restore')) {
+                return;
+            }
+            $table->tinyInteger('per_restore')->default(0)->after('vigor');
+        });
+        Schema::table('feats', function ($table) {
+            if (Schema::hasColumn('feats', 'per_restore')) {
+                return;
+            }
+            $table->tinyInteger('per_restore')->default(0)->after('per_day');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('skills', function (Blueprint $table) {
+            $table->dropColumn('per_restore');
+        });
+        Schema::table('feats', function (Blueprint $table) {
+            $table->dropColumn('per_restore');
+        });
+    }
+};
