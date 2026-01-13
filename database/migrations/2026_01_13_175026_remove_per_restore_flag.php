ad3e91e9e076afa9ddbfd049e06be5e7c902426a
Eligos Alexander <eligos.alexander@reasondigital.com>
2026-01-13 18:02:57 +0000

    remove per_restore flag

---
+++ database/migrations/2026_01_13_175026_remove_per_restore_flag.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        if (Schema::hasColumn('skills', 'per_restore')) {
+            Schema::table('skills', function (Blueprint $table) {
+                $table->dropColumn('per_restore');
+            });
+        }
+        if (Schema::hasColumn('feats', 'per_restore')) {
+            Schema::table('feats', function (Blueprint $table) {
+                $table->dropColumn('per_restore');
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('skills', function (Blueprint $table) {
+            if (Schema::hasColumn('skills', 'per_restore')) {
+                return;
+            }
+            $table->unsignedTinyInteger('per_restore')->default(0)->after('vigor');
+        });
+        Schema::table('feats', function (Blueprint $table) {
+            if (Schema::hasColumn('feats', 'per_restore')) {
+                return;
+            }
+            $table->unsignedTinyInteger('per_restore')->default(0)->after('per_day');
+        });
+    }
+};
