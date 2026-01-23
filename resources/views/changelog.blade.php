efa30854ecf156da106c218150a50e2bdabec43e
Eligos Alexander <eligos.alexander@reasondigital.com>
2026-01-23 19:25:56 +0000

    changelog

--- resources/views/changelog.blade.php
+++ resources/views/changelog.blade.php
@@ -9,9 +9,10 @@
     <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm lg:rounded-lg">
         <div class="p-6 text-gray-900 dark:text-gray-100 space-y-2">
             <div>
-                <h3 class="text-lg font-semibold">Update: 16th January 2026</h3>
+                <h3 class="text-lg font-semibold">Update: 23rd January 2026</h3>
                 <ul class="list-inside list-disc">
                     <li>Plot Co/Sys Ref: added skill specialty editing.</li>
+                    <li>Plot Co: increased size of downtime action responses.</li>
                     <li>Exposed skill and specialty data to players.</li>
                 </ul>
             </div>
