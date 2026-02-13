3ec282ab9ff9dbd72ca4f56ff218e9d55d0ba43d
Eligos Alexander <eligos.alexander@reasondigital.com>
2026-02-13 11:59:07 +0000

    update logo to use smaller sizes

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,1 +1,3 @@
-<img src="{{ asset('/images/sef-logo.png') }}" alt="{{ __('Stargate Expeditionary Force') }}" {{ $attributes }}/>
+<img src="{{ asset('/images/sef-logo-72.png') }}"
+     srcset="{{ asset('/images/sef-logo-72.png') }} 72w, {{ asset('/images/sef-logo-96.png') }} 96w, {{ asset('/images/sef-logo-128.png') }} 128w, {{ asset('/images/sef-logo-144.png') }} 144w, {{ asset('/images/sef-logo-152.png') }} 152w, {{ asset('/images/sef-logo-192.png') }} 192w, {{ asset('/images/sef-logo-384.png') }} 384w"
+     alt="{{ __('Stargate Expeditionary Force') }}" {{ $attributes }}/>
