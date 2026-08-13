37b6fdb75fcde3c4c735ac99d34ee6b56794c610
Eligos Alexander <eligos.alexander@reasondigital.com>
2026-08-13 16:55:55 +0100

    #127 fix teacher required training

--- resources/views/downtimes/partials/training-skills.blade.php
+++ resources/views/downtimes/partials/training-skills.blade.php
@@ -3,7 +3,7 @@
     @foreach ($character->trainingSkills as $skill)
         <option value="{{$skill->id}}"
                 @if (!empty($action->character_skill_id) && $skill->id == $action->character_skill_id) selected @endif
-                @if ($skill->skill->requires_teacher && !in_array($skill->id, $downtime->trainingCourseSkillIds)) disabled @endif
+                @if ($skill->skill->requires_teacher && !in_array($skill->skill_id, $downtime->trainingCourseSkillIds)) disabled @endif
         >
             {{ __(':name (:trained/:cost months):teacher', ['name' => $skill->name, 'trained' => $skill->trained, 'cost' => $skill->cost, 'teacher' => ($skill->skill->requires_teacher && !in_array($skill->id, $downtime->trainingCourseSkillIds) ? __(' - requires teacher') : '')]) }}
         </option>
